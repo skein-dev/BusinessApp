ef022841abc259e37405d4851ba68cd5ffcb56d2
skein <genji199544@gmail.com>
2021-03-23 16:57:11 +0900

    modify

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './PresentationalComponent'
 
-const PresentationalComponent = (props) => {
-   return (
-      <View>
-         <Text onPress = {props.updateState}>
-            {props.myState}
-         </Text>
-      </View>
-   )
+export default class App extends React.Component {
+   state = {
+      myState: 'This is my state'
+   }
+   render() {
+      return (
+         <View>
+            <PresentationalComponent myState = {this.state.myState}/>
+         </View>
+      );
+   }
 }
-export default PresentationalComponent
