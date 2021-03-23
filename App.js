b637427e1ab8a3e7bd8837970d9d368b1bcd79b7
skein <genji199544@gmail.com>
2021-03-23 17:03:23 +0900

    add style

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PresentationalComponent from './PresentationalComponent'
 
 export default class App extends React.Component {
    state = {
-      myState: "adimin"
-   }
-   updateState = () => {
-      this.setState({ myState: 'The state is updated' })
+      myState: 'This is my state'
    }
    render() {
       return (
          <View>
-            <PresentationalComponent sendState = {this.state.myState} updateState = {this.updateState}/>
+            <PresentationalComponent myState = {this.state.myState}/>
          </View>
       );
    }
--- PresentationalComponent.js
+++ PresentationalComponent.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const PresentationalComponent = (props) => {
    return (
       <View>
-         <Text onPress = {props.updateState}>
+         <Text style = {styles.myState}>
             {props.myState}
          </Text>
       </View>
    )
 }
 export default PresentationalComponent
+
+const styles = StyleSheet.create ({
+   myState: {
+      marginTop: 20,
+      textAlign: 'center',
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 20
+   }
+})
