2febea900c69affb496eefe177cc65b341134532
skein <genji199544@gmail.com>
2021-03-23 17:00:29 +0900

    app.js modify

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import PresentationalComponent from './PresentationalComponent'
 
 export default class App extends React.Component {
    state = {
-      myState: 'This is my state'
+      myState: "adimin"
+   }
+   updateState = () => {
+      this.setState({ myState: 'The state is updated' })
    }
    render() {
       return (
          <View>
-            <PresentationalComponent myState = {this.state.myState}/>
+            <PresentationalComponent sendState = {this.state.myState} updateState = {this.updateState}/>
          </View>
       );
    }
