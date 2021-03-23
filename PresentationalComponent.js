d29c515afd69fab7fc637f1dc4c3925c90c545aa
skein <genji199544@gmail.com>
2021-03-23 16:58:57 +0900

    modify

--- PresentationalComponent.js
+++ PresentationalComponent.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View } from 'react-native'
 
 const PresentationalComponent = (props) => {
    return (
       <View>
-         <Text style = {styles.myState}>
+         <Text onPress = {props.updateState}>
             {props.myState}
          </Text>
       </View>
    )
 }
 export default PresentationalComponent
-
-const styles = StyleSheet.create ({
-   myState: {
-      marginTop: 20,
-      textAlign: 'center',
-      color: 'blue',
-      fontWeight: 'bold',
-      fontSize: 20
-   }
-})
