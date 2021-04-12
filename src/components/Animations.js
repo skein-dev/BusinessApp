bc74a6dc97a2b4e500f58eb3359b53baa7317dd4
skein <genji199544@gmail.com>
2021-04-13 03:10:19 +0900

    animation modify

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -1,40 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, StyleSheet, Animated, TouchableOpacity } from 'react-native'
 
 class Animations extends Component {
-    componentWillMount = () => {
-        this.animatedWith = new this.animatedWith.Value( 50 )
-        this.animatedHeight = new this.animatedHeight.Value( 100 )
-    }
-    animatedBox = () => {
-        Animated.timing( this.animated, {
-            toValue: 200,
-            duration: 1000,
-        }).start()
-        Animated.timing( this.animated, {
-            toValue: 500,
-            duration: 500
-        }).start()
-    }
-    render() {
-        const animatedStyle = { width: this. animatedWidth, height: this.animatedHeight }
-        return (
-            <TouchableOpacity style = { style.container } onPress = { this.animatedBox } >
-                <Animated.View style = {[ style.box, animatedStyle ]}/>
-            </TouchableOpacity>
-        )
-    }
+   componentWillMount = () => {
+      this.animatedWidth = new Animated.Value(50)
+      this.animatedHeight = new Animated.Value(100)
+   }
+   animatedBox = () => {
+      Animated.timing(this.animatedWidth, {
+         toValue: 200,
+         duration: 1000
+      }).start()
+      Animated.timing(this.animatedHeight, {
+         toValue: 500,
+         duration: 500
+      }).start()
+   }
+   render() {
+       const animatedStyle = { width: this.animatedWidth, height: this.animatedHeight }
+       return (
+           <TouchableOpacity style = { styles.container } onPress = { this.animatedBox }>
+               <Animated.View style = { [styles.box, animatedStyle] } />
+           </TouchableOpacity>
+       )
+   }
 }
-export default Animations
+export default Animations 
 
-const styles = styleSheet.creat({
-    container: {
-        justifyContent: 'center',
-        aliginItem: 'center'
-    },
-    box: {
-        backgroundColor: 'blue',
-        width: 50,
-        height: 100,
-    }
+const styles = StyleSheet.create({
+   container: {
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   box: {
+      backgroundColor: 'blue',
+      width: 50,
+      height: 100
+   }
 })
